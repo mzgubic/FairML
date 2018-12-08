20c969d19b63b2e1d6c55b09ee066afa49cf9d03
mzgubic <miha.zgubic@gmail.com>
2018-12-08 11:46:39 +0100

    add the 2D adversary plot

--- fairmass/plotting.py
+++ fairmass/plotting.py
@@ -70,7 +70,69 @@ def plot_classifier_performance(data, pname='Clf_perf.pdf', batch=False):
     plt.close(fig)
 
 
+def plot_2D_GaussMix(fX, Y, Z, pname, batch=False):
+    
+    # percentiles
+    Z_median = np.percentile(Z, 50)
+    fX_median = np.percentile(fX, 50)
+    
+    # gaussian kernel density estimation
+    Z_plot = np.linspace(-4, 4, 100)
+    Z_log_dens = {}
+    Z_kde = KernelDensity(kernel='gaussian', bandwidth=0.1)
+    Z_log_dens['full'] = Z_kde.fit(Z.reshape(-1,1)).score_samples(Z_plot.reshape(-1,1))
+    Z_log_dens['low'] = Z_kde.fit(Z[(fX<fX_median).reshape(-1)].reshape(-1,1)).score_samples(Z_plot.reshape(-1,1))
+    Z_log_dens['high'] = Z_kde.fit(Z[(fX>fX_median).reshape(-1)].reshape(-1,1)).score_samples(Z_plot.reshape(-1,1))
+    
+    fX_plot = np.linspace(0, 1, 100)
+    fX_log_dens = {}
+    fX_kde = KernelDensity(kernel='gaussian', bandwidth=0.01)
+    fX_log_dens['full'] = fX_kde.fit(fX.reshape(-1,1)).score_samples(fX_plot.reshape(-1,1))
+    fX_log_dens['low'] = fX_kde.fit(fX[Z<Z_median].reshape(-1,1)).score_samples(fX_plot.reshape(-1,1))
+    fX_log_dens['high'] = fX_kde.fit(fX[Z>Z_median].reshape(-1,1)).score_samples(fX_plot.reshape(-1,1))
+    
+    # plot
+    fig, ax = plt.subplots(2, 2, figsize=(7,7), gridspec_kw={'height_ratios':[1,4], 'width_ratios':[4,1]}, sharex='col', sharey='row')
+    
+    # scatter
+    ax[1,0].fill_between([-4,Z_median], [0,0], [1,1], facecolor='tomato', interpolate=True, alpha=0.5)
+    ax[1,0].fill_between([Z_median,4], [0,0], [1,1], facecolor='darkred', interpolate=True, alpha=0.5)
+
+    indices = np.arange(0, len(Z), 500)
+    Z_sc, Y_sc, fX_sc = Z[indices], Y[indices], fX[indices]
+    col = np.where(fX_sc[Y_sc==0]<fX_median, 'royalblue', 'darkblue').reshape(-1)
+    ax[1,0].scatter(Z_sc[Y_sc==0], fX_sc[Y_sc==0], c=col, marker='.')
+    col = np.where(fX_sc[Y_sc==1]<fX_median, 'royalblue', 'darkblue').reshape(-1)
+    ax[1,0].scatter(Z_sc[Y_sc==1], fX_sc[Y_sc==1], c=col, marker='x')
+    
+    ax[1,0].set_xlim(-4, 4)
+    ax[1,0].set_ylim(0, 1)
+    ax[1,0].set_xlabel('Sensitive parameter Z')
+    ax[1,0].set_ylabel('f(X)')
+    
+    # top
+    ax[0,0].set_ylabel('p(z)')
+    ax[0,0].plot(Z_plot, np.exp(Z_log_dens['full']), color='k')
+    ax[0,0].plot(Z_plot, 0.5*np.exp(Z_log_dens['low']), color='royalblue')
+    ax[0,0].plot(Z_plot, 0.5*np.exp(Z_log_dens['high']), color='darkblue')
+    
+    # right
+    ax[1,1].set_xlabel('p(f(X))')
+    ax[1,1].plot(np.exp(fX_log_dens['full']), fX_plot, color='k')
+    ax[1,1].plot(0.5*np.exp(fX_log_dens['low']), fX_plot, color='tomato')
+    ax[1,1].plot(0.5*np.exp(fX_log_dens['high']), fX_plot, color='darkred')
+    
+    # empty
+    fig.delaxes(ax[0,1])
+    
+    plt.savefig(pname)
+    if not batch:
+        plt.show()
+    plt.close(fig)
+
+
 def plot_Z_density(Z, fX, NLLs, n_adv_cycles, n_curves=3, pname='Z_density.pdf', batch=False):
+
     # make the cuts
     boundaries = np.linspace(0, 1, n_curves+1)
     upper = np.array(boundaries[1:])
