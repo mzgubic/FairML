4a00ea439ebf5ecefa21850388509a7200c6fdf5
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-01-10 12:35:33 +0000

    fix gif making

--- scripts/HmumuTraining.py
+++ scripts/HmumuTraining.py
@@ -202,7 +202,7 @@ def train(args):
     #for pname in ['VarsComparison', 'MassCheck']:
     for pname in ['MassCheck']:
         dirn = 'media/plots/{p}/{d}'.format(p=pname, d=description)
-        in_pngs = ' '.join(['{d}/{n}_{c:03}.png'.format(d=dirn, n=description, c=c) for c in range(n_epochs)])
+        in_pngs = ' '.join(['{d}/{n}_{c:03}.png'.format(d=dirn, n=description, c=c) for c in range(n_epochs) if c%10==0])
         out_gif = 'media/gifs/{p}_{n}.gif'.format(p=pname, n=description)
         os.system('convert -colors 32 -loop 0 -delay 10 {i} {o}'.format(i=in_pngs, o=out_gif))
         print(out_gif)
