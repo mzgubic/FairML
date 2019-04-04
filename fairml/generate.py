92bf2e10ab6d7db9c31bc5b7ca4990131dca1038
mzgubic <miha.zgubic@gmail.com>
2019-04-04 10:09:47 +0100

    add exponential Z distros

--- fairml/generate.py
+++ fairml/generate.py
@@ -20,9 +20,9 @@ def toys_simple(n_samples, z=None):
 
     return {'X':X, 'Y':Y.reshape(-1, 1), 'Z':Z.reshape(-1, 1)}
 
-def toys_single_Z_diff(n_samples, z=None):
+def toys_expo_Z(n_samples, z=None):
 
-    sigma = 1
+    sigma = 0.5
     dx = 2
 
     # Y is the target
@@ -31,15 +31,10 @@ def toys_single_Z_diff(n_samples, z=None):
 
     # Z depends on Y (for training, can evaluate on any samples)
     if z == None:
-        # simple gaussian: Z|(Y==0) ~ N(0, 1)
-        Z0 = np.random.normal(0, sigma, size=n_samples//2)
 
-        # comes from one of the two: Z|(Y==1) ~ N(-1, 1) + N(1, 1)
-        Z1_1 = np.random.normal(-dx, sigma, size=n_samples//2)
-        Z1_2 = np.random.normal(dx, sigma, size=n_samples//2)
-        Z1_12 = np.concatenate([Z1_1, Z1_2]).reshape(2, -1)
-        index = np.random.randint(0, dx, n_samples//2)
-        Z1 = Z1_12[index, np.arange(len(index))]
+        # exponential
+        Z0 = np.random.exponential(0.5, size=n_samples//2)
+        Z1 = np.random.exponential(2.0, size=n_samples//2)
 
         # combine them
         Z = np.concatenate([Z0, Z1])
@@ -52,7 +47,7 @@ def toys_single_Z_diff(n_samples, z=None):
     # and Xs of course depend on both Z and Y
     X0 = np.random.multivariate_normal([0, 0], [[sigma, -.5*sigma], [-.5*sigma, sigma]], size=n_samples//2)
     X0[:, 1] += Z0
-    X1 = np.random.multivariate_normal([dx, 0], [[sigma, 0], [0, sigma]], size=n_samples//2)
+    X1 = np.random.multivariate_normal([dx, -1], [[sigma, 0], [0, sigma]], size=n_samples//2)
     X1[:, 1] += Z1
     X = np.concatenate([X0, X1])
 
